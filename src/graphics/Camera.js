5436076f6811d7de0e63b984d524bfea5b613c99
Hugo Collins <hugo@Bens-MacBook-Neo.local>
2026-07-27 21:05:18 -0400

    changed camera to implement Transform.js

--- src/graphics/Camera.js
+++ src/graphics/Camera.js
@@ -1,22 +1,23 @@
 import Mat4 from "../math/Mat4.js";
+import Transform from "../objects/Transform.js";
 
 export default class Camera {
 
     constructor() {
 
-        this.x = 0;
-        this.y = 0;
-
-        this.rotation = 0;
+        this.transform = new Transform();
 
     }
 
     getViewMatrix() {
 
         return Mat4
-            .rotationZ(-this.rotation)
+            .rotationZ(-this.transform.rotation.z)
             .multiply(
-                Mat4.translation(-this.x, -this.y, 0)
+                Mat4.translation(
+                    -this.transform.position.x,
+                    -this.transform.position.y,
+                    -this.transform.position.z)
             );
 
     }
